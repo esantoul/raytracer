5463aef57b8b42e4096c1ee125c8d6d92df70441
esantoul <e.santoul@gmail.com>
2020-12-16 18:59:31 +0100

    Improved Base arithmetics

--- base_arithmetics_helper.hpp
+++ base_arithmetics_helper.hpp
@@ -4,70 +4,72 @@ template <typename Source_t, typename Scalar_t>
 class Base_Arithmetics_Helper
 {
 public:
-  Source_t &operator+=(const Scalar_t &val)
+  // Single value compound assignment add
+  constexpr Source_t &operator+=(const Scalar_t &val)
   {
     return cmpd_assign(val, [](auto &l, const auto &r) { l += r; });
   }
-  Source_t &operator-=(const Scalar_t &val)
+  // Single value compound assignment subtract
+  constexpr Source_t &operator-=(const Scalar_t &val)
   {
     return cmpd_assign(val, [](auto &l, const auto &r) { l -= r; });
   }
-  Source_t &operator*=(const Scalar_t &val)
+  // Single value compound assignment multiply
+  constexpr Source_t &operator*=(const Scalar_t &val)
   {
     return cmpd_assign(val, [](auto &l, const auto &r) { l *= r; });
   }
-  Source_t &operator/=(const Scalar_t &val)
+  // Single value compound assignment divide
+  constexpr Source_t &operator/=(const Scalar_t &val)
   {
     return cmpd_assign(val, [](auto &l, const auto &r) { l /= r; });
   }
 
-  Source_t &operator+=(const Source_t &other)
+  // Single value add
+  constexpr Source_t operator+(const Scalar_t &val) { return Source_t{static_cast<const Source_t &>(*this)} += val; }
+  // Single value subtract
+  constexpr Source_t operator-(const Scalar_t &val) { return Source_t{static_cast<const Source_t &>(*this)} -= val; }
+  // Single value multiply
+  constexpr Source_t operator*(const Scalar_t &val) { return Source_t{static_cast<const Source_t &>(*this)} *= val; }
+  // Single value divide
+  constexpr Source_t operator/(const Scalar_t &val) { return Source_t{static_cast<const Source_t &>(*this)} /= val; }
+
+  // Elementwise compound assignment add
+  constexpr Source_t &operator+=(const Source_t &other)
   {
     return cmpd_assign_elwise(other, [](auto &l, const auto &r) { l += r; });
   }
 
-  Source_t &operator-=(const Source_t &other)
+  // Elementwise compound assignment subtract
+  constexpr Source_t &operator-=(const Source_t &other)
   {
     return cmpd_assign_elwise(other, [](auto &l, const auto &r) { l -= r; });
   }
 
-  Source_t &operator*=(const Source_t &other)
+  // Elementwise compound assignment multiply
+  constexpr Source_t &operator*=(const Source_t &other)
   {
     return cmpd_assign_elwise(other, [](auto &l, const auto &r) { l *= r; });
   }
 
-  Source_t &operator/=(const Source_t &other)
+  // Elementwise compound assignment divide
+  constexpr Source_t &operator/=(const Source_t &other)
   {
     return cmpd_assign_elwise(other, [](auto &l, const auto &r) { l /= r; });
   }
 
-  Source_t operator+(const Source_t &other)
-  {
-    Source_t ret{static_cast<const Source_t &>(*this)};
-    return ret += other;
-  }
-
-  Source_t operator-(const Source_t &other)
-  {
-    Source_t ret{static_cast<const Source_t &>(*this)};
-    return ret -= other;
-  }
-
-  Source_t operator*(const Source_t &other)
-  {
-    Source_t ret{static_cast<const Source_t &>(*this)};
-    return ret *= other;
-  }
-
-  Source_t operator/(const Source_t &other)
-  {
-    Source_t ret{static_cast<const Source_t &>(*this)};
-    return ret /= other;
-  }
+  // Elementwise add
+  constexpr Source_t operator+(const Source_t &other) { return Source_t{static_cast<const Source_t &>(*this)} += other; }
+  // Elementwise subtract
+  constexpr Source_t operator-(const Source_t &other) { return Source_t{static_cast<const Source_t &>(*this)} -= other; }
+  // Elementwise multiply
+  constexpr Source_t operator*(const Source_t &other) { return Source_t{static_cast<const Source_t &>(*this)} *= other; }
+  // Elementwise divide
+  constexpr Source_t operator/(const Source_t &other) { return Source_t{static_cast<const Source_t &>(*this)} /= other; }
 
 private:
   template <typename Fun_t>
-  Source_t &cmpd_assign(const Scalar_t &val, Fun_t &&op)
+  constexpr Source_t &cmpd_assign(const Scalar_t &val, Fun_t &&op)
   {
     Source_t &reference = static_cast<Source_t &>(*this);
     for (auto &el : reference)
@@ -76,7 +78,7 @@ private:
   }
 
   template <typename Fun_t>
-  Source_t &cmpd_assign_elwise(const Source_t &other, Fun_t &&op)
+  constexpr Source_t &cmpd_assign_elwise(const Source_t &other, Fun_t &&op)
   {
     Source_t &reference = static_cast<Source_t &>(*this);
     auto start = reference.begin();
