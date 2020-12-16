b1eadefed0c0622fc92dd55fb9cfb739e4dab480
esantoul <e.santoul@gmail.com>
2020-12-16 14:39:20 +0100

    added new arithmetic operators

--- base_arithmetics_helper.hpp
+++ base_arithmetics_helper.hpp
@@ -6,37 +6,39 @@ class Base_Arithmetics_Helper
 public:
   Source_t &operator+=(const Scalar_t &val)
   {
-    return cmpd_assign(val, [](auto &&l, auto &&r) { l += r; });
+    return cmpd_assign(val, [](auto &l, const auto &r) { l += r; });
   }
   Source_t &operator-=(const Scalar_t &val)
   {
-    return cmpd_assign(val, [](auto &&l, auto &&r) { l -= r; });
+    return cmpd_assign(val, [](auto &l, const auto &r) { l -= r; });
   }
   Source_t &operator*=(const Scalar_t &val)
   {
-    return cmpd_assign(val, [](auto &&l, auto &&r) { l *= r; });
+    return cmpd_assign(val, [](auto &l, const auto &r) { l *= r; });
   }
   Source_t &operator/=(const Scalar_t &val)
   {
-    return cmpd_assign(val, [](auto &&l, auto &&r) { l /= r; });
+    return cmpd_assign(val, [](auto &l, const auto &r) { l /= r; });
   }
 
   Source_t &operator+=(const Source_t &other)
   {
-    Source_t &reference = static_cast<Source_t &>(*this);
-    auto start = reference.begin();
-    for (const auto &el : other)
-      *(start++) += el;
-    return reference;
+    return cmpd_assign_elwise(other, [](auto &l, const auto &r) { l += r; });
   }
 
   Source_t &operator-=(const Source_t &other)
   {
-    Source_t &reference = static_cast<Source_t &>(*this);
-    auto start = reference.begin();
-    for (const auto &el : other)
-      *(start++) -= el;
-    return reference;
+    return cmpd_assign_elwise(other, [](auto &l, const auto &r) { l -= r; });
+  }
+
+  Source_t &operator*=(const Source_t &other)
+  {
+    return cmpd_assign_elwise(other, [](auto &l, const auto &r) { l *= r; });
+  }
+
+  Source_t &operator/=(const Source_t &other)
+  {
+    return cmpd_assign_elwise(other, [](auto &l, const auto &r) { l /= r; });
   }
 
   Source_t operator+(const Source_t &other)
@@ -51,16 +53,35 @@ public:
     return ret -= other;
   }
 
-  // TODO Implement hadamard_multiply
-  // TODO Implement hadamard_divide
+  Source_t operator*(const Source_t &other)
+  {
+    Source_t ret{static_cast<const Source_t &>(*this)};
+    return ret *= other;
+  }
+
+  Source_t operator/(const Source_t &other)
+  {
+    Source_t ret{static_cast<const Source_t &>(*this)};
+    return ret /= other;
+  }
 
 private:
   template <typename Fun_t>
-  Source_t &cmpd_assign(const Scalar_t &val, Fun_t &&fun)
+  Source_t &cmpd_assign(const Scalar_t &val, Fun_t &&op)
   {
     Source_t &reference = static_cast<Source_t &>(*this);
-    for (auto &&el : reference)
-      fun(el, val);
+    for (auto &el : reference)
+      op(el, val);
+    return reference;
+  }
+
+  template <typename Fun_t>
+  Source_t &cmpd_assign_elwise(const Source_t &other, Fun_t &&op)
+  {
+    Source_t &reference = static_cast<Source_t &>(*this);
+    auto start = reference.begin();
+    for (const auto &el : other)
+      op(*(start++), el);
     return reference;
   }
 };
